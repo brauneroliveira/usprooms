33fe6c3904db8d2898f973b5d2e9e23ab35077a4
brauneroliveira <brauner.rno@gmail.com>
2016-05-05 23:12:42 -0300

    imagem das salas no protótipo da página 1 (home)

--- views/site/index.php
+++ views/site/index.php
@@ -32,6 +32,7 @@ $this->title = 'Página 1: Página Inicial';
             foreach ($salas as $sala){
                 echo '<div class="col-lg-4">';
                 echo '<h3>' . $sala . '</h2>';
+                echo yii\helpers\Html::img('assets/images/lab-icmc.jpg');
                 echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
