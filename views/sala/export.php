225173c6cacd0b3e36c3014c10015c9d71ddad09
brauneroliveira <brauner.rno@gmail.com>
2016-05-11 02:04:45 -0300

    export xml/json (copy to clipboard)

--- views/sala/export.php
+++ views/sala/export.php
@@ -1,25 +1,34 @@
 <?php
 
 use yii\helpers\Html;
+use yii\web\View;
 
-/* @var $this yii\web\View */
-/* @var $model app\models\Sala */
-
-//$this->title = 'Update Sala: ' . $model->id_sala;
-//$this->params['breadcrumbs'][] = ['label' => 'Salas', 'url' => ['index']];
-//$this->params['breadcrumbs'][] = ['label' => $model->id_sala, 'url' => ['view', 'id' => $model->id_sala]];
-//$this->params['breadcrumbs'][] = 'Update';
+    $this->registerJsFile('http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js', ['position' =>View::POS_END]);
+    $this->registerCssFile('http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/default.min.css');
+    $this->registerJs('hljs.initHighlightingOnLoad();', View::POS_END);
+    $this->registerJsFile('https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.10/clipboard.min.js', ['position' => View::POS_END]);
 ?>
 <div class="sala-update">
-
-    <?php
     
-    echo \yii\bootstrap\Html::encode($xml);
+    <h2>XML</h2><?= Html::button('Copiar', ['id'=>'copiar-xml', 'class' => 'btn btn-info', 'data-clipboard-target'=> '#xml']) ?>
+    <pre>
+        <code id="xml" class="xml" style="font-size: medium;">
+            <?= Html::encode($xml); ?>
+        </code>
+    </pre>
+    <h2>JSON</h2><?= Html::button('Copiar', ['id'=>'copiar-json', 'class' => 'btn btn-info', 'data-clipboard-target'=> '#json']) ?>
+    <pre>
+        <code id="json" class="json" style="font-size: medium;">
+            <?= $json ?>
+        </code>
+    </pre>
     
-    echo '<br />';
-    
-    echo Html::encode($json);
-    
-    ?>
-
 </div>
+
+<?php
+
+    $this->registerJs("new Clipboard('#copiar-xml');", View::POS_END);
+    $this->registerJs("new Clipboard('#copiar-json');", View::POS_END);
+    
+
+?>
